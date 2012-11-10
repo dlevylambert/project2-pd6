18d1c4b63f019e270119db0353075c2083e117df
Sarah Robinson <robinsonsarah01@gmail.com>
2012-11-10 15:16:36 -0500

    added more stuff to proof.py

--- Group3/proof.py
+++ Group3/proof.py
@@ -10,7 +10,15 @@ if __name__ == "__main__":
 
     # nyc opendata - street events listing
     url = "http://data.cityofnewyork.us/api/views/xenu-5qjw/rows.json"
-    print getRes(url)['meta']['view']['description']
+    print getRes(url)['meta']['view']['name']
+
+    print "----------------"
+
+    url = "http://data.cityofnewyork.us/api/views/fiaa-wgtd/rows.json"
+    print getRes(url)['meta']['view']['name']
+
+    # note: google places and map APIs have limits
+    # after which, they charge you
 
     # heroku be there nyc 
     url = "http://betherenyc.com/api/search" #?date=2012-07-04"
