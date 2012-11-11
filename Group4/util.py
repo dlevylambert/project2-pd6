367280bad52a7615c7935f8294ca8ab3c6994560
Nathaniel Biggs <biggs0125@gmail.com>
2012-11-11 16:26:10 -0500

    small test

---
+++ Group4/util.py
@@ -0,0 +1,24 @@
+import urllib
+import json
+import sys
+from pymongo import Connection
+from twilio.rest import TwilioRestClient
+
+
+Conn = Connection('ds041367.mongolab.com',41367)
+db = Conn['stuycs_sideprojects']
+res = db.authenticate('stuycs','stuycs')
+coll = db.Project2
+
+account = 'ACd118dcbff7f480ecd7ba59b8c217e87f'
+token = '3acfc74ebcc11a59f4332bea592e7a1e'
+client = TwilioRestClient(account,token)   
+
+
+def getTextLog():
+    for message in client.sms.messages.list():
+        print message.body
+        #lines = message.body.split('\n')
+        #print lines[0]
+
+getTextLog()
