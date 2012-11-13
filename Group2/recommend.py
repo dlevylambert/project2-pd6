6bdeae25bf1120a67273cbf975cd88a88c693c24
Dina Levy-Lambert <dlevylambert@gmail.com>
2012-11-12 22:45:54 -0500

    asdasd

--- Group2/recommend.py
+++ Group2/recommend.py
@@ -1,11 +1,35 @@
+import json
+import urllib
+import sys
 from urllib2 import Request, urlopen
 
 id = 52591
 
-def get_similar_movies(id):
-    headers = {"Accept": "application/json"}
-    request = Request("http://themoviedb.apiary.io/3/movie/" + id + "/similar_movies", headers=headers)
-    response_body = urlopen(request).read()
-    print response_body
+class Rester:
+    def __init__(self, url):
+        self.url = url
+    def call(self, q):
+        urlstring = "%s?%s"%(self.url, q)
+        print urlstring
+        request = urllib.urlopen(urlstring)
+        result = json.loads(request.read())
+        return result
+    def authentication(self):
+        headers = {"Accept": "application/json"}
+        self.call("/3/authentication/token/new")
+    
 
-get_similar_movies(id)
+test = Rester("http://themoviedb.apiary.io")
+test.authentication()
+
+#result = test.call(qstring)
+#print result
+
+#def get_similar_movies(id):
+#    headers = {"Accept": "application/json"}
+#    request = Request("http://themoviedb.apiary.io/3/movie/" + str(id) + "/similar_movies", headers=headers)
+#    response_body = urlopen(request).read()
+#    print response_body
+
+#get_similar_movies(id)
+
