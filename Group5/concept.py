fccb5c82256457f6abf54581808a8e44373e577f
AKandola <amanpreet123@gmail.com>
2012-11-12 23:05:44 -0500

    still doesn't work

--- Group5/concept.py
+++ Group5/concept.py
@@ -7,13 +7,28 @@ import urllib2
 
 
 
-#returns stuff nearby the given latitude and longtitute
+#returns stuff nearby the given latitude and longtitute 
+#This is the general information of the area
 
-url="http://api.nytimes.com/svc/semantic/v2/geocodes/query.xml?nearby=38.920833,-94.622222_&feature_class=P&api-key=your-API-key (no space after comma between lat/lon)"
+url="http://api.nytimes.com/svc/semantic/v2/geocodes/query.json?nearby=38.920833,-94.622222_&feature_class=P&api-key=d575055f9ce4d2f35e76d42c3ff476c2:18:66928926"
 
 
 
-request=urllib.urlopen(url)
+#returns census information - I still need to get a key for this
+#gives the population of the current region (NYC)
+
+
+urltwo="http://api.usatoday.com/open/census/pop?keypat=New York&keyname=placename&sumlevid=3&api_key=XXXXXX"
+
+#google maps api
+
+#not sure how this works yet
+
+
+request=urllib2.urlopen(url)
 result=json.loads(request.read())
+print request
 
-print result
+request=urllib.urlopen(urltwo)
+#result=json.loads(request.read())
+#print result
