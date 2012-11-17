c36b7a789c959c157e92e17ea88cb04743ef0188
Nathaniel Biggs <biggs0125@gmail.com>
2012-11-17 01:48:42 -0500

    login

--- Group4/app.py
+++ Group4/app.py
@@ -12,19 +12,32 @@ def login():
     if request.method=='GET':
         return render_template('login1.html')
     else:
-        if request.form['login']=='Login':
+        if request.form.hasKey('login'):
             user = str(request.form['user'])
             password = str(request.form['pass'])
-            userdata = file('users.txt')
-            for line in userdata:
-                data = line.split(',')
-                if user == data[0].strip():
-                    if password == data[1].strip():
-                        session['user'] = user
-                        return redirect(url_for('menu'))
-                    else:
-                        break
+            if user in util.getUsernames():
+                if password == util.checkPassword(user):
+                    session['user'] = user
+                    return redirect(url_for('menu'))
             return render_template('login2.html')
+        if request.form.hasKey('newuser'):
+            return redirect(url_for('newuser'))
+
+@app.route('/newuser/',methods=['GET','POST'])
+def newuser():
+    if request.method=='GET':
+        return render_template('newuser.html',notmatching=False,taken=False)
+    else: 
+        if request.form.hasKey('submit'):
+            user = request.form['user']
+            password1 = request.form['pass1']
+            password2 = request.form['pass2']
+            if user in util.getUsernames():
+                return render_template('newuser.html',notmatching=False,taken=True)
+            if password1 != password2:
+                return render_template('newuser.html',notmatching=True,taken=False)
+            util.createNewUser(user,password1)
+            return redirect(url_for('login'))
             
 if __name__ == "__main__":
     app.debug = True 
--- Group4/util.py
+++ Group4/util.py
@@ -1,6 +1,8 @@
 import urllib
 import json
 import sys
+import time
+import base64
 from pymongo import Connection
 from twilio.rest import TwilioRestClient
 
@@ -8,12 +10,28 @@ from twilio.rest import TwilioRestClient
 Conn = Connection('ds041367.mongolab.com',41367)
 db = Conn['stuycs_sideprojects']
 res = db.authenticate('stuycs','stuycs')
-coll = db.Project2
+mongo = db.Project2
 
 account = 'ACd118dcbff7f480ecd7ba59b8c217e87f'
 token = '3acfc74ebcc11a59f4332bea592e7a1e'
 client = TwilioRestClient(account,token)   
 
+def createNewUser(user,password):
+    tmp = base64.b64encode(password)
+    newuser = {"user" : user, "pass" : tmp, "calinfo" : []}
+    mongo.insert(newuser)
+
+def checkPassword(user):
+    tmp = mongo.find_one({"user":user})
+    password = base64.b64decode(tmp['pass'])
+    return password
+
+def getUsernames():
+    tmp = mongo.find()
+    users = []
+    for line in tmp:
+        users.append(str(line['user']))
+    return users
 
 def getTextLog():
     for message in client.sms.messages.list():
@@ -21,3 +39,10 @@ def getTextLog():
         #lines = message.body.split('\n')
         #print lines[0]
 
+def getMostRecent():
+    updates = client.sms.messages.list()
+    recent = updates[0].body
+    print recent
+
+if __name__ == "__main__":
+    getMostRecent()
