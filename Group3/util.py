aed4227a19586f9a2b1b3a5846669152ef673d2d
Sam Vasilevskiy <sam@sam-ThinkPad-X61.(none)>
2012-11-19 17:31:53 -0500

    util.py completed

--- Group3/util.py
+++ Group3/util.py
@@ -1,8 +1,12 @@
 import xml.etree.ElementTree as ET
+import re
+import random
+
 
 categories = []
 tree = ET.parse("DPR_Eateries.xml")
 root = tree.getroot()
+
 
 def getCategories():
     for item in root[0]:
@@ -15,23 +19,39 @@ def getCategories():
             categories.append(s)
     return s
 
-getCategories()
 
 def search(inputstring):
+    inputstring = inputstring.lower()
     searchresults = []
+    strcounter = []
     strlist = inputstring.split(' ')
     for s in strlist:
-        for item in root[0]:
-            boolean = 0
-            for line in item:
+        strcounter.append(0)
+    for item in root[0]:
+        for line in item:
+            i = 0
+            for s in strlist:
                 if line.text != None:
-                    if line.text.split(s, 1)[0].__len__() == line.text.__len__():
-                        boolean = 1
-                for i in searchresults:
-                    if i[0].text == item[0].text:
-                        boolean = 0
-                if boolean == 1:
-                    searchresults.append(item)
+                    if re.search(s, line.text.lower()) != None:
+                        strcounter[i] += 1
+                i += 1
+        product = 1
+        for num in strcounter:
+            product *= num
+        if product > 0:
+            b = 0
+            for line in searchresults:
+                if item[0].text.lower() == line[0].text.lower():
+                    b = 1
+            if b == 0:
+                searchresults.append(item)
     return searchresults
-    
 
+
+def randomSearch(inputstring):
+    l = search(inputstring)
+    return l[random.randint(0,l.__len__() - 1)]
+
+ 
+def randomRestaurant():
+    return root[0][random.randint(0,root[0].__len__() - 1)]
