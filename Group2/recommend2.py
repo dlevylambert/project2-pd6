e4e722dcc68e8475aa717c8c9eb62227dfc2d61c
Dina Levy-Lambert <dlevylambert@gmail.com>
2012-11-23 17:31:48 -0500

    added an authenticate method, but am getting a weird error when adding the headers into URL

--- Group2/recommend2.py
+++ Group2/recommend2.py
@@ -6,6 +6,12 @@ from urllib2 import Request, urlopen
 API_KEY = "450062599145d021e7243a767de7c7d0"
 ID = 52591
 
+def authenticate(id):
+    global API_KEY
+    headers = {"Accept": "application/json"}
+    request = urllib.urlopen("http://api.themoviedb.org/3/authentication/token/new?api_key=" + API_KEY)
+    result = json.loads(request.read())
+    print result
 
 def get_similar_movies(id):
     global API_KEY
@@ -14,9 +20,11 @@ def get_similar_movies(id):
     response_body = urlopen(request).read()
     print response_body
 
+
 # + "&append_to_response=" + str(id) + "/similar_movies"
 
 
 if __name__ == "__main__":     
     get_similar_movies(ID)
+    authenticate(ID)
     
