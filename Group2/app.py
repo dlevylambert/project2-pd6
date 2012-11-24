750ae49d712d5a3fb9c98bdd0136fdc5d4b31125
Dina Levy-Lambert <dlevylambert@gmail.com>
2012-11-24 02:55:18 -0500

    asdasd

--- Group2/app.py
+++ Group2/app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def home():
     if request.method=="GET":
-        return render_template("moviechooser.html")
+        return render_template("moviechooser.html", genres = recommend2.get_genres())
     
 if __name__ == "__main__":
     app.run(debug = True)
