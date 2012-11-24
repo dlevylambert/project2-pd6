13755adfccbbbb5131f7b55a4dca8ac9c1e2b29f
NJacob <kn2ply@gmail.com>
2012-11-24 15:46:36 -0500

    added a bit more

--- Group3/app.py
+++ Group3/app.py
@@ -5,6 +5,9 @@ import util
 app=Flask(__name__)
 
 @app.route("/")
+@app.route("/index")
+@app.route("/index.html")
+@app.route("/home")
 def index():
     res = util.randomRestaurant()
     resName = res[0].text
@@ -13,7 +16,19 @@ def index():
     resLong = res[8].text
     #print resName,resLat,resLong,resLoc
     #return "<b>home</b>"
-    return render_template("test.html",resName=resName,resLat=resLat,
+    return render_template("index.html",resName=resName,resLat=resLat,
+                           resLong=resLong,resLoc=resLoc)
+
+@app.route("/locations")
+def locations():
+    res = util.randomRestaurant()
+    resName = res[0].text
+    resLoc = res[1].text
+    resLat = res[7].text
+    resLong = res[8].text
+    #print resName,resLat,resLong,resLoc
+    #return "<b>home</b>"
+    return render_template("res.html",resName=resName,resLat=resLat,
                            resLong=resLong,resLoc=resLoc)
 
 if __name__=="__main__":
