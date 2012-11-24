c4f36edc37db14bf1345c38b1b8180258d4bad9f
Dina Levy-Lambert <dlevylambert@gmail.com>
2012-11-24 00:56:23 -0500

    asdasd

--- Group2/recommend2.py
+++ Group2/recommend2.py
@@ -2,13 +2,30 @@ import json
 import urllib
 import sys
 from urllib2 import Request, urlopen
+from pymongo import Connection
+global connection
+global db
+global res
+global col
 
 API_KEY = "450062599145d021e7243a767de7c7d0"
 ID = 52591
 
 ####NOT SURE WE NEED AUTHENTICATE METHODS FOR WHAT WE DO. ALSO LOOK I MADE THE FIND MOVIE THING WORK SO NOW WE CAN GET THE JSON FROM IT
+
+def auth():
+    global connection
+    global db
+    global res
+    global col
+    connection = Connection('mongo.stuycs.org')
+    db = connection.admin
+    res = db.authenticate ('ml7', 'ml7')
+    db = connection['z-pd6']
+    col = db['movieRecommender']
     
 def get_similar_movies(movie):
+    auth()    
     global API_KEY
     headers = {"Accept": "application/json"}
     request = Request("http://api.themoviedb.org/3/movie/" + str(movie) + "?api_key=" + API_KEY + "&append_to_response=releases", headers=headers)
@@ -17,6 +34,7 @@ def get_similar_movies(movie):
     return result
 
 def get_movie(movie_name, id):
+    auth()
     global API_KEY
     headers = {"Accept": "application/json"}
     request = Request("http://api.themoviedb.org/3/search/movie?api_key=" + API_KEY + "&query=" + movie_name, headers=headers)
@@ -25,6 +43,7 @@ def get_movie(movie_name, id):
     return result
 
 def movie_info(movie_name, id):
+    auth()
     list_of_movies = get_movie(movie_name, id)
     counter = 0
     ids = []
@@ -39,8 +58,10 @@ def movie_info(movie_name, id):
     info.append(titles) #info[1] then refers to titles
     info.append(dates) #info[2] then refers to dates of release
     return info
-    
 
+def add_movie_database(movie_name, movie_id):
+    auth()
+    col.insert({'title': movie_name, 'id': movie_id})
     
 if __name__ == "__main__":     
     info = movie_info("uptown_girls",ID)
