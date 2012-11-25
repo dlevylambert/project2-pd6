ea4055c8625994c7f05eacf08125e7fadb4a814e
Dina Levy-Lambert <dlevylambert@gmail.com>
2012-11-24 22:42:50 -0500

    added more

--- Group2/app.py
+++ Group2/app.py
@@ -8,11 +8,28 @@ import recommend2
 from random import randrange
 
 app = Flask(__name__)
+global result
+result = {}
 
 @app.route("/", methods=["GET", "POST"])
 def home():
     if request.method=="GET":
         return render_template("moviechooser.html", genres = recommend2.get_genres())
+    else:
+        button = request.form["button"]
+        if button == "Search_Movie":
+            result = recommend2.movie_info(request.form["searchdata"])
+            return render_template("searchresults.html", searchResult = result)
+        if button == "Genre_Selection":
+            return render_template("searchresults.html", searchResult = result)
+        if button == "Latest_Selection":
+            return render_template("searchresults.html", searchResult = result)
+        if button == "Now_Playing_Selection":
+            return render_template("searchresults.html", searchResult = result)
+        if button == "Upcoming_Selection":
+            return render_template("searchresults.html", searchResult = result)
+        if button == "Popular_Selection":
+            return render_template("searchresults.html", searchResult = result)
     
 if __name__ == "__main__":
     app.run(debug = True)
