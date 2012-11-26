c65277c3cc3fda053826eec51253cf8274c40d64
Dina Levy-Lambert <dlevylambert@gmail.com>
2012-11-25 21:28:22 -0500

    dasda

--- Group2/app.py
+++ Group2/app.py
@@ -23,19 +23,19 @@ def home():
         if button == "Genre_Selection":
             res = request.form["genre_selection"]
             result = recommend2.get_movies_by_genre(res)
-            return render_template("searchresults.html", searchResult = result)
+            return render_template("searchresults.html", genreResult = result)
         if button == "Latest_Selection":
             result = recommend2.latest_movies()
-            return render_template("searchresults.html", searchResult = result)
+            return render_template("searchresults.html", latestResult = result)
         if button == "Now_Playing_Selection":
             result = recommend2.now_playing_movies()
-            return render_template("searchresults.html", searchResult = result)
+            return render_template("searchresults.html", playingResult = result)
         if button == "Upcoming_Selection":
             result = recommend2.upcoming_movies()
-            return render_template("searchresults.html", searchResult = result)
+            return render_template("searchresults.html", upcomingResult = result)
         if button == "Popular_Selection":
             result = popular_movies()
-            return render_template("searchresults.html", searchResult = result)
+            return render_template("searchresults.html", popularResult = result)
     
 if __name__ == "__main__":
     app.run(debug = True)
