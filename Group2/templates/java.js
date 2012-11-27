8e66f6edcac7417ae83ccfefb2ab8a6e93ad4250
Jasmine Li <jasmineli4444@gmail.com>
2012-11-26 23:09:35 -0500

    changed

--- Group2/templates/java.js
+++ Group2/templates/java.js
@@ -1,31 +1,7 @@
-function genre() {
-    searchResult = get_genres.result
-}
-
-function latest() {
-    searchResult = latest_movies.result
-}
-
-function nowplaying() {
-    searchResult = now_playing_movies.result
-}
-
-function upcoming() {
-    searchResult = upcoming_movies.result
-}
-
-function popular() {
-    searchResult = popular_movies.result
-}
-
-function searching() {
-}
-
 $(document).ready(function(){
-    $("#Genre_Selection").click(genre);
-    $("#Latest_Selection").click(latest);
-    $("#Now_Playing_Selection").click(nowplaying);
-    $("#Upcoming_Selection").click(upcoming);
-    $("#Popular_Selection").click(popular);
-    $("#Search_Movie").click(searching);
+    $("#movielist").click(summary);
 });
+
+function summary(e){
+
+}
