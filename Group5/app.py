6e3c153741cebffaaa30354437f8bab3e3136fd5
allidoiswin <fallenpwr@gmail.com>
2012-11-27 12:11:58 -0500

    k

--- Group5/app.py
+++ Group5/app.py
@@ -7,3 +7,7 @@ app = Flask(__name__)
 def home():
 	if request.method == 'GET':
 		return render_template('home.html')
+
+if __name__=="__main__":
+    app.debug=True # remove this line to turn off debugging
+    app.run()
