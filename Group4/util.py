0e513eacef38a089d906121bb3537b63d7aa7427
Michael Cohen <budjmt@aol.com>
2012-11-27 12:19:07 -0500

    oops

--- Group4/util.py
+++ Group4/util.py
@@ -24,7 +24,7 @@ phonenum = '+16468074041'
 def createNewUser(user,password,number):
     tmp = base64.b64encode(password)
     number = str(number).strip(' -+_()')
-    newuser = {"user" : user, "pass" : tmp, "number" : number, "calinfo" : {'2012' : {'1':{},'2':{},'3':{},'4':{},'5':{},'6':{},'7':{},'8':{},'9':{},'10':{},'11':{},'12':{}},"reminderTime":"8:00","remindersEnabled":True}
+    newuser = {"user" : user, "pass" : tmp, "number" : number, "calinfo" : {'2012' : {'1':{},'2':{},'3':{},'4':{},'5':{},'6':{},'7':{},'8':{},'9':{},'10':{},'11':{},'12':{}}},"reminderTime":"8:00","remindersEnabled":True}
     mongo.insert(newuser)
 
 def checkPassword(user):
