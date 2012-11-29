616aadb894fc85e72074b4f26a2192d566fa95b3
Eric Cerny <ericc6134@gmail.com>
2012-11-28 22:24:32 -0500

    edited templates, fixed app.py syntax

--- Group0/app.py
+++ Group0/app.py
@@ -4,19 +4,25 @@ from flask import session, escape
 from flask import request
 from flask import render_template
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 @app.route("/", methods = ["GET", "POST"])
 def homepage():
         if request.method == "GET":
-            return render_template("musicboxhome.html");
+		return render_template("musicboxhome.html");
         else:
-            button = request.form["button"]
-            if button == 'Home':
-                return redirect(url_for('moviechooser'))
-            if button == 'About Us'
-                return redirect(url_for('aboutus'))
+		button = request.form["button"]
+		if button == 'Home':
+			return redirect(url_for('moviechooser'))
+		if button == 'About Us':
+			return redirect(url_for('aboutus'))
+		
+@app.route("/aboutus.html",methods=["GET"])
+def aboutus():
+	return render_template("aboutus.html")
+
 #will add search later
 
 if __name__ == "__main__":
     app.debug = True
+    app.run();
