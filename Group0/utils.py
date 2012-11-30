42fb0c3d265fed63baea14859a090772de1a01e5
Eric Cerny <ericc6134@gmail.com>
2012-11-29 21:45:10 -0500

    working on add new user

---
+++ Group0/utils.py
@@ -0,0 +1,21 @@
+from pymongo import Connection
+
+conn = Connection("mongo.stuycs.org")
+def connect():
+    db = conn.admin
+    res = db.authenicate("ml7","ml7")
+
+def add_user(username):
+    db = conn["musicbox"]
+    users = db.first_collection
+    entry = {"name": username, "songs": []}
+    users.insert(entry)
+    info = [line for line in users.find()]
+    print info
+
+def get_songs(username):
+    db = conn["musicbox"]
+    users = db.first_colletion
+    for line in users.find():
+        if line["name"] == username:
+            return line["songs"]
