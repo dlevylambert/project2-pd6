ab21cf3bbbaef62d4373bef1f7f6baaa31f0d6ed
Amanda <vamandaboss1@gmail.com>
2012-12-01 17:01:44 -0500

    updated

--- Group0/musicservices.py
+++ Group0/musicservices.py
@@ -3,7 +3,9 @@ import json
 from pymongo import Connection
 from urllib2 import Request, urlopen
 global connection, db, res, collection
+#import discogs_client
 
+discogs.user_agent = 'musicbox'
 #api_key = "ZPNZ4NM30OA84ZR2"
 api_url = 'http://api.discogs.com'
 def beg():
@@ -13,30 +15,37 @@ def beg():
     res = db.authenticate ('ml7', 'ml7')
     db = connection['z-pd6']
     collection = db['musicservices']
+    
 
-def getAlbuminfo(artist_name):
+def getArtistinfo(artist_name):
     headers = {"Accept" : "application/json"}
-    request = Request("http://api.discogs.com/artist/" + artist_name)
+    request = Request("http://api.discogs.com/artist/" + artist_name, headers = headers)
+    response_body = urlopen(request).read()
+    result = json.loads(response_body)
+    return result #Artistinfo gives ID as well so someone can search releases!
+    # artist = discogs.Artist('artist_name').data
+    #print artist
+def getArtistinfoID(artist_id):
+    headers = {"Accept" : "application/json"}
+    request = Request("http://api.discogs.com/artists/" + (str)artist_id, headers = headers)
     response_body = urlopen(request).read()
     result = json.loads(response_body)
     return result
-#class Artist(APIBase):
-    #def __init__(self, name, anv=None):
-        #self._id = name
-        #self._aliases = []
-        #self._namevariations = []
-        #self._releases = []
-        #self._anv = anv or None
-        #APIBase.__init__(self)
-   
 
-   # @property
-    #def aliases(self):
-        #if not self._aliases:
-           # for alias in self.data.get('aliases', []):
-              #  self._aliases.append(Artist(alias))
-        #return self._aliases
+def getReleasesID(artist_id):
+    headers = {"Accept" : "application/json"}
+    request = Request("http://api.discogs.com/artists/" + (str)artist_id +"/releases", headers = headers)
+    response_body = urlopen(request).read()
+    result = json.loads(response_body)
+    return result
 
+def getcertainrelease(release_id):
+    headers = {"Accept" : "application/json"}
+    request = Request("http://api.discogs.com/artists/" + (str)release_id +"/releases", headers = headers)
+    response_body = urlopen(request).read()
+    result = json.loads(response_body)
+    return result # can get from artist' releases
+    #release = discogs.Release(artist_id)
+    #release.tracklist
 
-#will work on more later
-#url of site is http://freemusicarchive.org/api
+#if __name__ == "__main__":
