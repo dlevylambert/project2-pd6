e2a3aa054d8185a3376b30bb581e36a1678fecf0
Daniel Teehan <drteehan@gmail.com>
2012-12-02 14:03:19 -0500

    updated access of data

--- Group1/util.py
+++ Group1/util.py
@@ -15,16 +15,23 @@ def getSchools():
 
 
 def testing2():
-    if int(data[0][11], base=10) == 391:
+    if int(data[0][11]) == 391:
         print "testing2 works"
 
 #gives an error saying int() can't convert non-string with explicit base?
 def testing():
     i = 0
     for item in data:
-        if int(data[i][11], base=10) > 600:
-            print data[i][9]
-        i = i + 1
+        print item
+        print item[9]  #prints school name
+        if item[11] != None:  #prints critical reading score if it exists
+            print int(item[11])
+        else:
+            print item[11]
+        #this doesnt work
+        #if int(data[i][11]) > 600:
+        #    print data[i][9]
+        #i = i + 1
 
 if __name__ == "__main__":
     getSchools()
