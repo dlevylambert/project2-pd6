2ebf74773ad4cd9ca8520f8017d7e72790dbf740
Kevin <kevin@kevin-HP-Pavilion-dm4-Notebook-PC.(none)>
2012-12-02 09:39:56 -0500

    stuff

--- Group5/app.py
+++ Group5/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route("/", methods=['GET', 'POST'])
 def home():
 	if request.method == 'GET':
-		return render_template('home.html')
+		return render_template('game.html')
 
 if __name__=="__main__":
     app.debug=True # remove this line to turn off debugging
