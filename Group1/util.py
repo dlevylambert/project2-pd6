f26e0f0a60d14aa01822a5469236b93c4402d430
Daniel Teehan <drteehan@gmail.com>
2012-12-02 14:24:11 -0500

    put all of the schools into a shelve

--- Group1/util.py
+++ Group1/util.py
@@ -1,17 +1,45 @@
 #!/usr/bin/python
-
+import shelve
 import urllib2
 import urllib
 import json
 
 satScores = json.loads(urllib2.urlopen("http://data.cityofnewyork.us/api/views/zt9s-n5aj/rows.json").read())
 data = satScores["data"]
+p = shelve.open("storage")
+
+def stackShelve():
+    print "a"
+    school=''
+    info=[]
+    temp = []
+    for item in data:
+        info = []
+        school=str(item[9])
+        if item[11] != None:
+            temp = int(item[11])
+        else:
+            temp = item[11]
+        info.append(temp)
+        if item[12] != None:
+            temp = int(item[12])
+        else:
+            temp = item[12]
+        info.append(temp)
+        if item[13] != None:
+            temp = int(item[13])
+        else:
+            temp = item[13]
+        info.append(temp)
+        p[school] = info
+    print p.keys()
+    print p[p.keys()[1]]
+
 
 def getSchools():
-    i = 0
     for item  in data:
-        print data[i][9]
-        i = i + 1
+        print item[9]
+        
 
 
 def testing2():
@@ -34,6 +62,7 @@ def testing():
         #i = i + 1
 
 if __name__ == "__main__":
-    getSchools()
-    testing2()
-    testing()
+    stackShelve()
+    #getSchools()
+    #testing2()
+    #testing()
