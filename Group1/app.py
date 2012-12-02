8febad86d3bbff602ad606afe6a39ed70287a263
Oliver Ball <oball.stuy@gmail.com>
2012-12-01 22:21:25 -0500

    general work

--- Group1/app.py
+++ Group1/app.py
@@ -13,31 +13,49 @@ login_manager.setup_app(app)
 
 app.secret_key = 'secret key'
 
+#Login work starts here
+#
 @login_manager.user_loader
 def load_user(userid):
     return User.get(userid)
+
+
+
+#
+#Login work ends here
 
 @app.route("/", methods=["GET", "POST"])
 def home():
     if request.method=="GET":
         return render_template("default.html", )
     if request.method=="POST":
-        #need to discuss use of login
         pass
+        
 
 @app.route("/index", methods=["GET", "POST"])
 def index():
     if request.method=="GET":
-        pass
+        if 'username' in session:
+            return render_template("index.html", username = session['username'])
+        else:
+            return render_template("index.html")
     if request.method=="POST":
-        pass
+        email = request.form['email']
+        password = request.form['password']
+        
+        #return 'EMAIL: ' + email+ '<p>Password: ' + password
     
 @app.route("/search", methods=["GET", "POST"])
 def search():
     if request.method=="GET":
-        pass
+        return render_template("search.html")
     if request.method=="POST":
-        pass
+        #This can change depending on how we make the search process work,
+        #but I thought I'd just put something up to work with.
+        #The options are either:
+        # 1. A whole new page (more html work)
+        # 2. Keep the same page and change it using javascript
+        return render_template("madeSearch.html", variousinformation='information passed on from search.html')
     
 @app.route("/mySearches")
 @login_required
